093709ee25b7b6bd25d57ee4b3200fe6a2bbdc09
RahmanVahabov <rahmanrv@code.edu.az.com>
2022-04-04 01:23:56 +0400

    homework 01.04.22

---
+++ classwork_1.04.22/claswork_02_04_22.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+namespace classwork_1._04._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region task1
+
+            /*int a = 5;
+            int b = 5;
+            int c = 5;
+
+            if (a > b && a > c)
+            {
+                Console.WriteLine("en boyuk a-dir ");
+            }
+
+            else if (b > a && b > c)
+            {
+                Console.WriteLine("en boyuk b-dir");
+            }
+
+            else if (c > a && c > b)
+            {
+                Console.WriteLine("en oyuk c-dir");
+            }
+            else
+            {
+                Console.WriteLine("beraberlik");
+            }*/
+
+            #endregion
+
+            #region task2
+            /*Console.WriteLine("1-ci ededi daxil edin");
+            string a = Console.ReadLine();
+            int nums1 = int.Parse(a);
+
+
+
+            Console.WriteLine("2-ci ededi daxil edin");
+            string b = Console.ReadLine();
+            int nums2 = int.Parse(b);
+
+            Console.WriteLine("3-ci ededi daxil edin");
+            string c = Console.ReadLine();
+            int nums3 = int.Parse(c);
+
+
+            Console.WriteLine("4-ci ededi daxil edin");
+            string d = Console.ReadLine();
+            int nums4 = int.Parse(d);
+            
+
+
+            Console.WriteLine("5-ci ededi daxil edin");
+            string e = Console.ReadLine();
+            int nums5 = int.Parse(e);
+
+            Console.WriteLine("ededlerin ededi ortasi");
+            int cem = (nums1 + nums2 + nums3 + nums4 + nums5) / 5;
+            Console.WriteLine(cem);
+            */
+            #endregion
+
+            #region task3
+            /*Console.WriteLine("duzbucaqlinin enini daxil edin");
+            string a = Console.ReadLine();
+            int en = int.Parse(a);
+
+            Console.WriteLine("duzbucaqlinin uzunlugunu daxil edin");
+            string b = Console.ReadLine();
+            int uz = int.Parse(b);
+
+            int s = (en * uz);
+            Console.WriteLine($"sahesi {s}");
+
+            int p = (en + uz) * 2;
+            Console.WriteLine($"perimetri {p}");
+            */
+            #endregion
+
+            #region task5
+            /*Console.WriteLine("eded daxil edin");
+            string a = Console.ReadLine();
+            int b = int.Parse(a);
+            int count = 0;
+
+            while (b > 0)
+            {
+                b = b / 10;
+                count++;
+            }
+            Console.WriteLine($"mertebelidir {count}");
+            */
+            #endregion
+
+            #region task6
+            /*Console.WriteLine("eded daxil et");
+            string a = Console.ReadLine();
+            int natural = int.Parse(a);
+
+            if (natural > 1 && natural % 21 == 0)
+            {
+                
+            Console.WriteLine("eded hem 3-e hemde 7-ye qaliqsiz bolunur ");
+            */
+            #endregion
+
+            #region task7
+           /*Console.WriteLine("bal daxil et");
+            string a = Console.ReadLine();
+            int bal = int.Parse(a);
+
+            if (bal >= 91 && bal <= 100)
+            {
+                Console.WriteLine("sizin qiymetiniz A-dir");
+            }
+            else if (bal >= 81 && bal <= 90)
+            {
+                Console.WriteLine("sizin qiymetiniz B-dir");
+            }
+            else if (bal >= 71 && bal <= 80)
+            {
+                Console.WriteLine("sizin qiymetiniz C-dir");
+
+            }
+            else if (bal >= 61 && bal <= 70)
+            {
+                Console.WriteLine("sizin qiymetiniz D-dir");
+
+            }
+            else if (bal >= 51 && bal <= 60)
+            {
+                Console.WriteLine("sizin qiymetiniz E-dir");
+
+            }
+            else
+            {
+                Console.WriteLine("bextivi bir daha sina keilmisen");
+                */
+                #endregion
+            }
+        }
+            
+        }
+    }
+    
+    
+}
